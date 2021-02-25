c5b70accce6bb7542c083dfd74ba2aa1219cb5b7
Juan Jose Ramirez <juanjose.rami25@gmail.com>
2021-02-24 23:37:18 -0300

    :sparkles: update the banck account, adding the user

--- index.py
+++ index.py
@@ -1,18 +1,19 @@
 class User:
     
-    def __init__(self,name,email):
+    def __init__(self,name,email,account_balance):
         self.name = name
         self.email = email
         self.account_balance = BankAccount(int_rate = 0.02, balance = 0)
-
-
-
-
+    
+    def example_method(self):
+        self.account_balance.deposit(100)
+        print(self.account_balance)
+        
 
 class BankAccount: 
     
-    def __init__(self,username,in_rate,account_balance):
-        self.name = username
+    def __init__(self,name,in_rate,account_balance):
+        self.name = name
         print("Hello Welcome", self.name,"to the banck account", account_balance)
 
     def deposit(self):
@@ -35,7 +36,11 @@ class BankAccount:
     def yiel_interest(self):
         pass
 
-us = BankAccount('Juan Jose',0.02,100000)
-us.deposit()
-us.withdraw()
-us.display_account_info()
+juanjose = User('Juan Jose','juanjose@hello.com')
+print(juanjose)
+
+# us = BankAccount('Juan Jose',0.02,100000)
+# us.deposit()
+# us.withdraw()
+# us.display_account_info()
+
